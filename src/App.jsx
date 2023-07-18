087702035bf643f45a8f84162832fd4024f1fbbd
Roshan Acharya <roshanacharya207@gmail.com>
2023-07-18 20:22:06 +0545

    portfolio

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react' //rafce to create functional component
+import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+
+const App = () => {
+  return (
+    <>
+    <Header />
+    <Nav /> 
+    </>
+  )
+}
+
+export default App
---
+++ src/components/about/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './about.css'
+
+const About = () => {
+  return (
+    <div> About
+    <p> Welcome to my profile</p> </div>
+  )
+}
+
+export default About
