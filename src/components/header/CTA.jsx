497f04c10d7fa19b6ab920c2bde31cd0e560a6c7
Roshan Acharya <roshanacharya207@gmail.com>
2023-07-20 18:13:31 +0545

    modified

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import CV from '../../assets/cv.pdf'
+import './header.css'
 
 const CTA = () => {
   return (
-    <div classname='cta'>
+    <div className='cta'>
         <a href={CV} download className='btn'>Download CV</a>
         <a href='#contact' className='btn btn-primary'>Let's Talk</a>
     </div>
